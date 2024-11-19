10cfaf473aba06cc864d37bdd62821bbe4552833
Nidhi Rajawat <nidhirajawat321@gmail.com>
2024-11-19 18:21:33 +0530

    updated

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,10 +1,9 @@
-// components/TodoList.js
 import React, { useState } from "react";
 import { PiCheck, PiPencil, PiX } from "react-icons/pi";
 import { useDispatch, useSelector } from "react-redux";
 import { removeTodo, markCompleted } from "../../features/todo/todoSlice";
 import ConfirmationModal from "../../features/todo/ConfirmationModal";
-
+import "./TodoList.css"; // Import your custom CSS file
 
 export const TodoList = ({ onEdit }) => {
   const taskList = useSelector((state) => state.todoReducer.todoList);
@@ -12,6 +11,9 @@ export const TodoList = ({ onEdit }) => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [taskToDelete, setTaskToDelete] = useState(null);
+
+  // State to control the visibility of the Todo list
+  const [isTodoListVisible, setIsTodoListVisible] = useState(true);
 
   const handleTaskActions = (task, type = "mark-complete") => {
     if (type === "mark-complete") {
@@ -32,28 +34,53 @@ export const TodoList = ({ onEdit }) => {
     setIsModalOpen(false); // Close modal
   };
 
+  // Show Todo List Button handler
+  const showTodoList = () => {
+    setIsTodoListVisible(true); // Set the Todo list to visible
+  };
+
+  // Hide Todo List Button handler
+  const hideTodoList = () => {
+    setIsTodoListVisible(false); // Set the Todo list to hidden
+  };
+
   return (
     <div className="todo-list">
-      <ul>
-        {taskList &&
-          taskList.map((task, index) => (
-            <li key={`list_item${index}`}>
-              {task?.id} - {task?.title} - {task?.description}
-              <span>{task?.status}</span>
-              <div className="todo-actions">
-                <button onClick={() => handleTaskActions(task)}>
-                  <PiCheck />
-                </button>
-                <button onClick={() => handleTaskActions(task, "edit")}>
-                  <PiPencil />
-                </button>
-                <button onClick={() => handleTaskActions(task, "delete")}>
-                  <PiX />
-                </button>
-              </div>
-            </li>
-          ))}
-      </ul>
+      {/* Separate Show and Hide buttons */}
+      {!isTodoListVisible && (
+        <button className="todo-button show" onClick={showTodoList}>
+          Show Todo List
+        </button>
+      )}
+      {isTodoListVisible && (
+        <button className="todo-button hide" onClick={hideTodoList}>
+          Hide Todo List
+        </button>
+      )}
+
+      {/* Conditionally render the Todo list */}
+      {isTodoListVisible && (
+        <ul>
+          {taskList &&
+            taskList.map((task, index) => (
+              <li key={`list_item${index}`}>
+                {task?.id} - {task?.title} - {task?.description}
+                <span>{task?.status}</span>
+                <div className="todo-actions">
+                  <button onClick={() => handleTaskActions(task)}>
+                    <PiCheck />
+                  </button>
+                  <button onClick={() => handleTaskActions(task, "edit")}>
+                    <PiPencil />
+                  </button>
+                  <button onClick={() => handleTaskActions(task, "delete")}>
+                    <PiX />
+                  </button>
+                </div>
+              </li>
+            ))}
+        </ul>
+      )}
 
       {/* Confirmation Modal */}
       <ConfirmationModal
